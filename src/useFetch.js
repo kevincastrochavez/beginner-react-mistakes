0d85e10df45b939a097586b81909f37d09a7900a
Kevin Castro <cas20057@byui.edu>
2023-08-26 23:35:12 -0600

    learned how to handle multiple fast fetch requests

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,12 +6,24 @@ export function useFetch(url) {
   const [error, setError] = useState();
 
   useEffect(() => {
+    const controller = new AbortController();
     setLoading(true);
-    fetch(url)
+    fetch(url, { signal: controller.signal })
       .then(setData)
       .catch(setError)
       .finally(() => setLoading(false));
+
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
   return { loading, data, error };
 }
+
+// Multiple urls fired too soon
+// Second fetch could be the one taking more time, ended up with that result
+// Data not synced
+// 1.0ms + 150ms = 150ms
+// 2.100ms + 300ms = 400ms
+// 3.200ms + 100ms = 300ms
